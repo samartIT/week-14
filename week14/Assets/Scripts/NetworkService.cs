9c9e0fb69d81cea1e6b17e947cb9dd1616a8e718
it59070034 <42163063+it59070034@users.noreply.github.com>
2019-04-24 10:16:19 +0700

    Part I

---
+++ week14/Assets/Scripts/NetworkService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkService
+{
+    private const string xmlAPI = "http://api.openweathermap.org/data/2.5/weather?q=bangkok,th&mode=xml&APPID=5feb1ab3693c126280171ed3f424a5a5";
+    private IEnumerator CallAPI(string url, Action<string> callback) {
+        using (UnityWebRequest request = UnityWebRequest.Get(url)) {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError)
+            {
+                Debug.LogError("network problem : " + request.error);
+            }
+            else if (request.responseCode != (long)System.Net.HttpStatusCode.OK)
+            {
+                Debug.LogError("response error : " + request.responseCode);
+            }
+            else {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+    public IEnumerator GetWeatherXML(Action<string> callback) {
+        return CallAPI(xmlAPI, callback);
+    }
+}
---
+++ week14/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    [SerializeField] private Light sun;
+
+    private float _fullIntensity;
+    private float _cloudValue = 0f;
+
+    void Awake()
+    {
+        Messenger.AddListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated);
+    }
+
+    void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated);
+    }
+
+    void Start()
+    {
+        _fullIntensity = sun.intensity;
+    }
+
+    void OnWeatherUpdated() {
+        SetOvercast(Managers.Weather.cloudValue);
+    }
+
+    //void Update()
+    //{
+     //   SetOvercast(_cloudValue);
+      //  _cloudValue += 0.005f;
+    //}
+
+    private void SetOvercast(float value) {
+        sky.SetFloat("_Blend", value);
+        sun.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+}
