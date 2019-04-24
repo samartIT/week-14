1236b2715eaedd83c0ab3e29f928ea6f8461d12b
PeetTharathanakul <peet12131424@gmail.com>
2019-04-24 10:42:27 +0700

    *

---
+++ week14/Assets/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    [SerializeField] private Light sun;
+    // Start is called before the first frame update
+    private float _fullIntensity;
+    private float _cloudValue = 0f;
+
+    void Start()
+    {
+        _fullIntensity = sun.intensity;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        SetOvercast(_cloudValue);
+        _cloudValue += 0.005f;
+    }
+    private void SetOvercast(float value)
+    {
+        sky.SetFloat("_Blend", value);
+        sun.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+}
