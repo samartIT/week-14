362b978f79a3613cb5d44335dd65968bb606ed3c
it59070060 <bookinsapl2@gmail.com>
2019-04-29 23:27:15 +0700

    LabWeather-done

---
+++ week14/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    [SerializeField] private Light sun;
+
+    private float _fullIntensity;
+
+    void Awake()
+    {
+        Messenger.AddListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated);
+    }
+    void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.WEATHER_UPDATED, OnWeatherUpdated);
+    }
+
+    void Start()
+    {
+        _fullIntensity = sun.intensity;
+    }
+
+    // Update is called once per frame
+    void OnWeatherUpdated()
+    {
+        SetOvercast(Managers.Weather.cloundValue);
+    }
+
+    private void SetOvercast(float value)
+    {
+        sky.SetFloat("_Blend", value);
+        sun.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+}
---
+++ week14/Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml;
+
+public class WeatherManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+    private NetworkService _network;
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Weather starting ... ");
+        _network = service;
+        StartCoroutine(_network.GetWeatherXML(OnXMLDataLoaded));
+        status = ManagerStatus.Initializing;
+    }
+    public float cloundValue { get; private set;}
+    public void OnXMLDataLoaded(string data)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(data);
+        XmlNode root = doc.DocumentElement;
+        XmlNode node = root.SelectSingleNode("clouds");
+        string value = node.Attributes["value"].Value;
+        cloundValue = Convert.ToInt32(value) / 100f;
+        Debug.Log("Value = " + cloundValue);
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
+
+        status = ManagerStatus.Started;
+    }
+}
