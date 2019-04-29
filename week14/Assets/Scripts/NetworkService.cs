132af3910a445697c38637bb79d1fa2194f579c7
Pai <59070155@it.kmitl.ac.th>
2019-04-29 20:19:48 +0700

    ddddd

---
+++ week14/Assets/Scripts/NetworkService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkService
+{
+    private const string xmlAPI = "http://api.openweathermap.org/data/2.5/weather?q=bangkok,th&mode=xml&APPID=f28ac29d58733b62581e9056b9db4936";
+
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError)
+            {
+                Debug.LogError("network problem : " + request.url);
+            }else if (request.responseCode != (long)System.Net.HttpStatusCode.OK)
+            {
+                Debug.LogError("network problem : " + request.error);
+            }
+            else
+            {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(xmlAPI, callback);
+    }
+}
---
+++ week14/Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+
+    private NetworkService _network;
+
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Weather strating ... ");
+        _network = service;
+        StartCoroutine(_network.GetWeatherXML(OnXMLDataLoaded));
+        status = ManagerStatus.Initializing;
+    }
+
+    public void OnXMLDataLoaded(string data)
+    {
+        Debug.Log(data);
+        status = ManagerStatus.Started;
+    }
+}
