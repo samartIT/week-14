a427f0a882bdc55d3afde4f1368c96dd9a4648a3
ITSupapitch <59070169@it.kmitl.ac.th>
2019-04-30 17:23:40 +0700

    Lab_wk14_updated

---
+++ week14/Assets/Scripts/NetworkService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkService : MonoBehaviour
+{
+private const string xmlAPI = "http://api.openweathermap.org/data/2.5/weather?q=bangkok,th&mode=xml&APPID=e8c14da326ca8e84d9672a5e0321acef";
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+
+            if (request.isNetworkError)
+            {
+                Debug.LogError("network problem : " + request.error);
+            } else if (request.responseCode != (long)System.Net.HttpStatusCode.OK)
+            {
+                Debug.LogError("response error : " + request.responseCode);
+            }
+            else
+            {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(xmlAPI, callback);
+    }
+
+}
---
+++ week14/Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml;
+
+public class WeatherManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+
+    private NetworkService _network;
+
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Weather starting ...");
+        _network = service;
+        StartCoroutine(_network.GetWeatherXML(OnXMLDataLoaded));
+        status = ManagerStatus.Initializing;
+    }
+
+    public void OnXMLDataLoaded(string data)
+    {
+        Debug.Log(data);
+    }
+
+    public float cloudValue { get; private set; }
+    public void onXMLDataLoaded(string data)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(data);
+        XmlNode root = doc.DocumentElement;
+        XmlNode node = root.SelectSingleNode("clouds");
+        string value = node.Attributes["value"].Value;
+        cloudValue = Convert.ToInt32(value) / 100f;
+        Debug.Log("Value = " + cloudValue);
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATE);
+
+        status = ManagerStatus.Started;
+    }
+}
