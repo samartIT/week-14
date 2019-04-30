9471da27c9870b155a872366554ccde5ba0bf111
59070052 <59070052@kmitl.ac.th>
2019-04-30 21:41:40 +0700

    up

--- week14/Assets/Scripts/AudioManager.cs
+++ week14/Assets/Scripts/AudioManager.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status { get; private set; }
-    private NetworkService _netWork;
-
-    public float soundVolumn
+    private NetworkService _network;
+    [SerializeField] private AudioSource soundSrc;
+    public void PlaySound(AudioClip clip)
     {
-        get { return AudioListener.volume;}
-        set { AudioListener.volume = value;}
+        soundSrc.PlayOneShot(clip);
     }
 
+    public  float soundVolumn
+    {
+        get { return AudioListener.volume; }
+        set { AudioListener.volume = value; }
+    }
     public bool soundMute
     {
         get { return AudioListener.pause; }
         set { AudioListener.pause = value; }
     }
-    
-    public void Starup(NetworkService service)
+
+    public void Startup(NetworkService service)
     {
         Debug.Log("Audio Manager starting ...");
-        _netWork = service;
+        _network = service;
         soundVolumn = 1f;
         status = ManagerStatus.Started;
     }
-
-    
 }
--- week14/Assets/Scripts/SettingsPopup.cs
+++ week14/Assets/Scripts/SettingsPopup.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class SettingsPopup : MonoBehaviour
 {
+    [SerializeField] private AudioClip sound;
+
     public void OnSoundToggle()
     {
         Managers.Audio.soundMute = !Managers.Audio.soundMute;
+        Managers.Audio.PlaySound(sound);
     }
 
     public  void OnSoundValue(float volume)
