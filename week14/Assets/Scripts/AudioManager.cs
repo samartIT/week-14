0a9025c63708c2eeffbd9e078361ae3f9ea9ba2c
Pai <59070155@it.kmitl.ac.th>
2019-05-01 00:14:02 +0700

    Finish

--- week14/Assets/Scripts/AudioManager.cs
+++ week14/Assets/Scripts/AudioManager.cs
@@ -5,24 +5,70 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour, IGameManager
 {
     [SerializeField] private AudioSource soundSrc;
-    [SerializeField] private AudioClip music;
-    private void Start()
-    {
-        Managers.Audio.PlaySound(music);
-    }
+    [SerializeField] private AudioSource music1Src;
+    [SerializeField] private string introBGMusic;
+    [SerializeField] private string levelBGMusic;
+
     public void PlaySound(AudioClip clip)
     {
         soundSrc.PlayOneShot(clip);
     }
+    public void PlayIntroMusic()
+    {
+        PlayMusic(Resources.Load("Music/" + introBGMusic) as AudioClip);
+    }
+    public void PlayLevelMusic()
+    {
+        PlayMusic(Resources.Load("Music/" + levelBGMusic) as AudioClip);
+    }
+    private void PlayMusic(AudioClip clip)
+    {
+        music1Src.clip = clip;
+        music1Src.Play();
+    }
+    public void StopMusic()
+    {
+        music1Src.Stop();
+    }
+
+
     public ManagerStatus status { get; private set; }
     private NetworkService _network;
+    private float _musicVolume;
     public float soundVolumn
     {
         get { return AudioListener.volume; }
         set { AudioListener.volume = value; }
     }
+    public float musicVolume
+    {
+        get { return _musicVolume; }
+        set
+        {
+            _musicVolume = value;
+            if (music1Src != null)
+            {
+                music1Src.volume = _musicVolume;
+            }
+        }
+    }
 
     public bool soundMute
+    {
+        get {
+            if (music1Src != null)
+            {
+                return music1Src.mute;
+            }
+            return false;
+        }
+        set { if (music1Src != null)
+            {
+                music1Src.mute = value;
+            }
+                }
+    }
+    public bool musicMute
     {
         get { return AudioListener.pause; }
         set { AudioListener.pause = value; }
@@ -32,7 +78,11 @@ public class AudioManager : MonoBehaviour, IGameManager
     {
         Debug.Log ("Audio Manager starting ...");
         _network = service;
+        music1Src.ignoreListenerVolume = true;
+        music1Src.ignoreListenerPause = true;
+
         soundVolumn = 1f;
+        musicVolume = 1f;
         status = ManagerStatus.Started;
     }
 }
--- week14/Assets/Scripts/SettingPopup.cs
+++ week14/Assets/Scripts/SettingPopup.cs
@@ -10,9 +10,33 @@ public class SettingPopup : MonoBehaviour
         Managers.Audio.soundMute = !Managers.Audio.soundMute;
         Managers.Audio.PlaySound(sound);
     }
+    public void OnMusicToggle()
+    {
+        Managers.Audio.musicMute = !Managers.Audio.musicMute;
+        Managers.Audio.PlaySound(sound);
+    }
 
     public void OnSoundValue(float volume)
     {
         Managers.Audio.soundVolumn = volume;
+    }
+    public void OnMusicValue(float volume)
+    {
+        Managers.Audio.musicVolume = volume;
+    }
+    public void OnPlayMusic(int selector)
+    {
+        switch (selector)
+        {
+            case 1:
+                Managers.Audio.PlayIntroMusic();
+                break;
+            case 2:
+                Managers.Audio.PlayLevelMusic();
+                break;
+            default:
+                Managers.Audio.StopMusic();
+                break;
+        }
     }
 }
