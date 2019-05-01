bb2c79ebafca23052bd657147746e877c79338de
The buddy_G's <59070144@it.kmitl.ac.th>
2019-05-01 11:47:07 +0700

    Week 14 - Part 2/2
    ใส่เพลงลงไปในเกม

--- week14/Assets/Scripts/AudioManager.cs
+++ week14/Assets/Scripts/AudioManager.cs
@@ -4,11 +4,56 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour, IGameManager
 {
+    [SerializeField] private AudioSource soundSrc;
+    [SerializeField] private AudioSource music1Src;
+    [SerializeField] private string introBGMusic;
+    [SerializeField] private string levelBGMusic;
+
+    public void PlayIntroMusic()
+    {
+        PlayMusic(Resources.Load("Music/" + introBGMusic) as AudioClip);
+    }
+
+    public void PlayLevelMusic()
+    {
+        PlayMusic(Resources.Load("Music/" + levelBGMusic) as AudioClip);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        music1Src.clip = clip;
+        music1Src.Play();
+    }
+
+    public void StopMusic()
+    {
+        music1Src.Stop();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        soundSrc.PlayOneShot(clip);
+    }
+
     public ManagerStatus status { get; private set; }
     private NetworkService _network;
+
+    public float soundVolumn
+    {
+        get { return AudioListener.volume; }
+        set { AudioListener.volume = value; }
+    }
+
+    public bool soundMute
+    {
+        get { return AudioListener.pause; }
+        set { AudioListener.pause = value; }
+    }
+
     public void Startup(NetworkService service) {
         Debug.Log ("Audio Manager starting ...");
         _network = service;
+        soundVolumn = 1f;
         status = ManagerStatus.Started;
     }
     // Start is called before the first frame update
---
+++ week14/Assets/Scripts/SettingPopup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingPopup : MonoBehaviour
+{
+    [SerializeField] private AudioClip sound;
+
+    public void OnSoundToggle()
+    {
+        Managers.Audio.soundMute = !Managers.Audio.soundMute;
+        Managers.Audio.PlaySound(sound);
+    }
+
+    public void OnSoundValue(float volume)
+    {
+        Managers.Audio.soundVolumn = volume;
+    }
+
+    public void OnPlayMusic(int selector)
+    {
+        Managers.Audio.PlaySound(sound);
+        switch (selector)
+        {
+            case 1:
+                Managers.Audio.PlayIntroMusic();
+                break;
+            case 2:
+                Managers.Audio.PlayLevelMusic();
+                break;
+            default:
+                Managers.Audio.StopMusic();
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
